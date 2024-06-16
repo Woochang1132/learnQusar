59a9df05b2cb54b647ca86965ad24d8ea0c8443b
Woochang1132 <show4031132@gmail.com>
2024-06-16 15:20:44 +0900

    [qusar] > 퀘이사에서 제공하는 colors 설정 추가하기

--- src/pages/Colors.vue
+++ src/pages/Colors.vue
@@ -7,19 +7,34 @@
         <div class="q-pa-xs rounded-borders bg-primary text-yellow">
           primary
         </div>
-        <div class="bg-secondary">secondary</div>
-        <div class="bg-accent">accent</div>
-        <div class="bg-dark">dark</div>
-        <div class="bg-positive">positive</div>
-        <div class="bg-negative">negative</div>
-        <div class="bg-info">info</div>
-        <div class="bg-warning">warning</div>
-        <div class="bg-purple-5">purple-5</div>
-        <div class="bg-pink-7">pink-7</div>
-        <div class="bg-light-blue">light-blue</div>
+        <div class="q-pa-md rounded-borders bg-secondary text-white">
+          secondary
+        </div>
+        <div class="q-pa-md rounded-borders bg-accent">accent</div>
+        <div class="q-pa-md rounded-borders bg-dark">dark</div>
+        <div class="q-pa-md rounded-borders bg-positive">positive</div>
+        <div class="q-pa-md rounded-borders bg-negative">negative</div>
+        <div class="q-pa-md rounded-borders bg-info">info</div>
+        <div class="q-pa-md rounded-borders bg-warning">warning</div>
+        <div class="q-pa-md rounded-borders bg-purple-5">purple-5</div>
+        <div class="q-pa-md rounded-borders bg-pink-7">pink-7</div>
+        <div class="q-pa-md rounded-borders bg-error">error</div>
         <p>Variables</p>
       </div>
       <q-btn color="yellow" text-color="black" label="검정" />
+    </section>
+    <section class="q-mb-xl">
+      <div class="text-h4">components</div>
+      <q-separator class="q-my-md" />
+      <q-btn color="white" text-color="black" label="Standard" />
+      <q-btn color="primary" label="Primary" />
+      <q-btn color="secondary" label="Secondary" />
+      <q-btn color="amber" glossy label="Amber" />
+      <q-btn color="brown-5" label="Brown 5" />
+      <q-btn color="deep-orange" glossy label="Deep Orange" />
+      <q-btn color="purple" label="Purple" />
+      <q-btn color="black" label="Black" />
+      <q-btn color="error" label="Error" />
     </section>
   </q-page>
 </template>
@@ -30,5 +45,11 @@
 p {
   color: $red;
   background-color: $grey-5;
+}
+.text-error {
+  color: $red;
+}
+.bg-error {
+  background: $red;
 }
 </style>
